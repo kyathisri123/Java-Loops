231cf93a1dae0de5906e34004599b8459d53ab14
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:16:23 +0530

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,18 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt(), sum = 0, temp = num;
+        while (temp > 0) {
+            int digit = temp % 10;
+            sum += digit * digit * digit;
+            temp /= 10;
+        }
+        System.out.println(num + " is " + (sum == num ? "an Armstrong number." : "not an Armstrong number."));
+        sc.close();
+    }
+}
