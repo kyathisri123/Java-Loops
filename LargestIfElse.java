8c7802eedf756afa2966291f861c5750cf45243e
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:22:42 +0530

    Add files via upload

---
+++ LargestIfElse.java
@@ -0,0 +1,20 @@
+package Loops; 
+
+import java.util.Scanner;
+
+public class LargestIfElse {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        
+        if (a >= b && a >= c) {
+            System.out.println("Largest number is: " + a);
+        } else if (b >= a && b >= c) {
+            System.out.println("Largest number is: " + b);
+        } else {
+            System.out.println("Largest number is: " + c);
+        }
+        sc.close();
+    }
+}
