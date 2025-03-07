11e1b1fe9c2f3fcf43ea40f07e8d9ceb25019eee
kyathisri123 <kyathi2025@gmail.com>
2025-03-07 16:23:05 +0530

    Add files via upload

---
+++ LargestNumber.java
@@ -0,0 +1,14 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class LargestNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter three numbers: ");
+        int a = sc.nextInt(), b = sc.nextInt(), c = sc.nextInt();
+        int largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+        System.out.println("Largest number is: " + largest);
+        sc.close();
+    }
+}
